13ada801a10e059a7c6d188cd4632cf979da5a93
aeshapatel025 <aesha.patel@spreadd.io>
2023-12-05 16:36:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Main.scss';
+import btnStyles from './Button.module.css';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to My React App</h1>
+      </header>
+      <main>
+        <section>
+          <p>This is a simple React app. Feel free to explore!</p>
+        </section>
+        <section>
+          <h2>Buttons with Styles</h2>
+          <button className={btnStyles.success}>SUCCESS</button>
+          <button className={btnStyles.error}>ERROR</button>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // Import MiniCssExtractPlugin
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      clean: true,
+    }),
+    new MiniCssExtractPlugin(), // Initialize MiniCssExtractPlugin
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      }, // Added missing closing brace here
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { modules: true } },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { modules: true } },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
+};
