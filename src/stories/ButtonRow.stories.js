77bb289e40a9f243d99107c3ad3cc8997547a76f
aeshapatel025 <aesha.patel@spreadd.io>
2023-12-08 18:57:01 +0530

    add cards components using storybook

--- src/stories/ButtonRow.stories.js
+++ src/stories/ButtonRow.stories.js
@@ -1,6 +1,6 @@
 // src/stories/ButtonRow.stories.js
 import React from 'react';
-import { ButtonRow } from '../ButtonRow';
+import { ButtonRow } from '../ButtonRow'; // Correct import path
 
 export default {
   title: 'ButtonRow',
