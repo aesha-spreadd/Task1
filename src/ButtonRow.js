cd67bba9bfe16aeafa0f750ebf24760af1de04db
aeshapatel025 <aesha.patel@spreadd.io>
2023-12-08 13:07:09 +0530

    add button using storybook

---
+++ src/ButtonRow.js
@@ -0,0 +1,14 @@
+// src/ButtonRow.js
+import React from 'react';
+import { ButtonRowContainer, Button } from './styled';
+
+export const ButtonRow = () => {
+  return (
+    <ButtonRowContainer>
+      <Button color="primary">PRIMARY BUTTON</Button>
+      <Button color="secondary">SECONDARY BUTTON</Button>
+      <Button color="success">SUCCESS BUTTON</Button>
+      <Button color="warning">WARNING BUTTON</Button>
+    </ButtonRowContainer>
+  );
+};
---
+++ src/stories/ButtonRow.stories.js
@@ -0,0 +1,10 @@
+// src/stories/ButtonRow.stories.js
+import React from 'react';
+import { ButtonRow } from '../ButtonRow';
+
+export default {
+  title: 'ButtonRow',
+  component: ButtonRow,
+};
+
+export const FourButtons = () => <ButtonRow />;
