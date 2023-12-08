d8fd0ce0325ddf77cb7b16535d164e31cfd998df
aeshapatel025 <aesha.patel@spreadd.io>
2023-12-08 14:22:25 +0530

    create outlineButton

--- src/styled.js
+++ src/styled.js
@@ -1,5 +1,5 @@
 // src/styled.js
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const ButtonRowContainer = styled.div`
   display: flex;
@@ -12,19 +12,93 @@ export const Button = styled.button`
   cursor: pointer;
   font-size: 14px;
   border-radius: 4px;
+  background-color: white;
+  border: 1px solid black;
+  color: black;
 
-  ${(props) => {
-    switch (props.color) {
-      case 'primary':
-        return 'background-color: red; color: white;';
-      case 'secondary':
-        return 'background-color: white; color: black; border: 1px solid black;';
-      case 'success':
-        return 'background-color: green; color: white;';
-      case 'warning':
-        return 'background-color: orange; color: white;';
-      default:
-        return '';
-    }
-  }}
+  &:hover {
+    background-color: transparent;
+  }
+
+  ${(props) =>
+    props.color &&
+    css`
+      &:hover {
+        background-color: ${props.color};
+        color: white;
+      }
+    `}
+
+  &:active {
+    background-color: ${(props) => (props.color ? props.color : 'transparent')};
+    color: white;
+  }
+
+  ${(props) =>
+    props.variant === 'outlined' &&
+    css`
+      background-color: transparent;
+      border: 1px solid ${props.color || 'black'};
+      color: ${props.color || 'black'};
+    `}
+
+  ${(props) =>
+    props.variant === 'floating' &&
+    css`
+      position: relative;
+      z-index: 1;
+      box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+    `}
+
+  ${(props) =>
+    props.color === 'primary' &&
+    css`
+      border-color: red;
+      color: red;
+
+      &:hover,
+      &:active {
+        background-color: red;
+        color: white;
+      }
+    `}
+
+  ${(props) =>
+    props.color === 'warning' &&
+    css`
+      border-color: orange;
+      color: orange;
+
+      &:hover,
+      &:active {
+        background-color: orange;
+        color: white;
+      }
+    `}
+
+  ${(props) =>
+    props.color === 'success' &&
+    css`
+      border-color: green;
+      color: green;
+
+      &:hover,
+      &:active {
+        background-color: green;
+        color: white;
+      }
+    `}
+
+  ${(props) =>
+    props.color === 'secondary' &&
+    css`
+      border-color: grey;
+      color: grey;
+
+      &:hover,
+      &:active {
+        background-color: grey;
+        color: white;
+      }
+    `}
 `;
