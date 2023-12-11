b3180c6c1f486ffc5c3e980677184afc8b58b3e6
aeshapatel025 <aesha.patel@spreadd.io>
2023-12-11 14:34:48 +0530

    Add a Buttoncard component in a storybook

---
+++ src/stories/ButtonCard.stories.js
@@ -0,0 +1,10 @@
+// ButtonCard.stories.js
+import React from 'react';
+import ButtonCard from '../Model/ButtonCard';
+
+export default {
+  title: 'ButtonCard',
+  component: ButtonCard,
+};
+
+export const ButtonCardStory = () => <ButtonCard />;
